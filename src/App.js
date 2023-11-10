d411167bca848347e3b6de3857cba689cc61d801
CaroJimenez <20213tn053@utez.edu.mx>
2023-11-10 10:06:41 -0600

    plantilla de login no terminada

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+
+  const handleClick = () => {
+    console.log('Iniciar sesión');
+    // Puedes agregar lógica adicional aquí
+  };
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src='/libro.png'/>
+        <div className='form'>
+          <div className='textContainer'>
+          <a className='text'>Correo</a>
+          </div>
+          <div className='inputContainer'>
+            <input></input>
+          </div>
+          <div className='passwordContainer'>
+          <a className='text'>Contraseña</a>
+          </div>
+          <div className='inputContainer'>
+          <input
+              type={passwordVisible ? 'text' : 'password'}
+              placeholder='Contraseña'
+            />
+          <button onClick={togglePasswordVisibility}>
+              {passwordVisible ? 'Ocultar' : 'Mostrar'}
+            </button>
+          
+            
+          </div>
+          <div className='buttonContainer'>
+          <button className='buttonLogin' onClick={handleClick}>Iniciar sesión</button>
+          </div>
+
+          <div className='linkContainer'>
+          <a className='textLink'>¿Olvidaste tu contraseña?</a>
+          </div>
+          <a className='textLink'>Registrarse</a>
+          
+
+        </div>
       </header>
     </div>
   );
