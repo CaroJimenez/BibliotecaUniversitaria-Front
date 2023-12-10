51e9380a817d39cbed72fa93e40cbc6fd00e7e1c
CaroJimenez <20213tn053@utez.edu.mx>
2023-12-10 10:55:06 -0600

    correccion de ruta en app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import ListBooks from "./views/books/List_books";
-import Login from "./views/login/Login"
+import ListBooks from "./views/books/list_books";
+import Login from "./views/login/Login";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Login/>
+      element: <Login />,
     },
     {
       path: "/list_books",
-      element: <ListBooks/>
-    }
-  ])
+      element: <ListBooks />,
+    },
+  ]);
 
-  return(
-   <React.StrictMode>
-    <RouterProvider router={router}>
-      {/* Nota según la documentación podemos rencerizar cmponentes comunes aquí, ejemnplo:
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router}>
+        {/* Nota según la documentación podemos rencerizar cmponentes comunes aquí, ejemnplo:
       <Header/> */}
-    </RouterProvider>
-   </React.StrictMode>
-  )
+      </RouterProvider>
+    </React.StrictMode>
+  );
 }
 
 export default App;
